e68c22fd788a742e9d1b615f7b2c1b1a2133eb72
Timothy McReynolds <timothy.mcreynold@selu.edu>
2020-09-19 21:48:12 -0500

    Updated Model Class to have an it value for all of the attributes of the property

--- msmpm/Models/Property.cs
+++ msmpm/Models/Property.cs
@@ -7,9 +7,25 @@ namespace MSMBackend.Models
 {
     public class Property
     {
-        public long Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
-        public int RoofRating { get; set; }
-        public int BuildingRating { get; set; }
+        public string Location { get; set; }
+        public string ImageURL { get; set; }
+        public Boolean Utilities { get; set; }
+        //public int SteveID { get; set; }
+        public int Roof { get; set; }
+        public int ExtWalls { get; set; }
+        public int ExtOpenings { get; set; }
+        public int Framework { get; set; }
+        public int Piers { get; set; }
+        public int Chimney { get; set; }
+        public int Door { get; set; }
+        public int Windows { get; set; }
+        public int Shutters { get; set; }
+        public int Flooring { get; set; }
+        public int Electrical { get; set; }
+        public int Plumbing { get; set; }
+        public int Sewer { get; set; }
+        public int HVAC { get; set; }
     }
 }
