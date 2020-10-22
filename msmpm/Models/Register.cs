6f5cddf1cf3f0c21caf8fce43cbee6f7a8d52619
kennethcole5 <Kenneth.cole@selu.edu>
2020-10-22 10:19:42 -0500

    IdentityModelsPtII

---
+++ msmpm/Models/Register.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSMBackend.Models
+{
+    public class Register
+    {
+        public int Id { get; set; }
+        
+        public String Email { get; set }
+
+        public String Password { get; set }
+
+        public String EmployeeName { get; set }
+
+        public String City { get; set }
+
+        public String Department { get; set }
+    }
+}
---
+++ msmpm/Models/Response.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSMBackend.Models
+{
+    public class Response
+    {
+        public String Status { get; set; }
+
+        public String Message { get; set; }
+    }
+}
