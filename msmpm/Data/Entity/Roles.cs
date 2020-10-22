bd1fb04e5817fb70e9bc31d05281d35fc9454975
Cameron Bonura <cameron.bonura@selu.edu>
2020-10-22 18:18:01 -0500

    Starting on Roles

---
+++ msmpm/Data/Entity/Roles.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MSMBackend.Data.Entity
+{
+    internal static class Roles
+    {
+        public const string Admin = nameof(Admin);
+        public const string Editor = nameof(Editor);
+        public const string Viewer = nameof(Viewer);
+
+        private static bool HasAnyRole(ClaimsPrincipal user, string target)
+        {
+            foreach(var role in target.Split(","))
+            {
+                if (user.IsInRole(role))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
