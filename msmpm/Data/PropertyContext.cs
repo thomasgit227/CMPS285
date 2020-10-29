43681f707461379dca99bb99bebbc09036280936
kennethcole5 <Kenneth.cole@selu.edu>
2020-10-29 12:59:06 -0500

    Cleaned up DBContext with ModelBuilderExtension
    So to not have all the seed data cluttering up the DB Context, the ModelBuilderExtension class in the Models folder can hold all the information about the Seed, and we can just call modelBuilder.Seed() in the DBContext

--- msmpm/Data/PropertyContext.cs
+++ msmpm/Data/PropertyContext.cs
@@ -16,33 +16,7 @@ namespace MSMBackend.Models
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Property>().HasData(
-                    new Property
-                    {
-                        Id = 2,
-                        Name = "TestProp",
-                        Location = "TestLocation",
-                        Username = "TestUsername",
-                        ImageURL = "TestImage",
-                        Utilities = false,
-                        Roof = 7,
-                        ExtWalls = 7,
-                        ExtOpenings = 7,
-                        Framework = 7,
-                        Piers = 7,
-                        Chimney = 7,
-                        Door = 7,
-                        Windows = 7,
-                        Shutters = 7,
-                        Flooring = 7,
-                        Electrical = 7,
-                        Plumbing = 7,
-                        Sewer = 7,
-                        HVAC = 7,
-                        EditTime = DateTime.Now
-
-                    }
-                 );
+            modelBuilder.Seed();
         }
     }
 }
