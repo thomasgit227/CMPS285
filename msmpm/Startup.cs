741aa0415fc0d69a8606d60379bed94c40c9e088
Cameron Bonura <cameron.bonura@selu.edu>
2020-11-05 15:40:09 -0600

    Swagger is Installed in Dev

--- msmpm/Startup.cs
+++ msmpm/Startup.cs
@@ -46,6 +46,11 @@ namespace MSMBackend
             services.AddSpaStaticFiles(configuration => {
                 configuration.RootPath = "MSMPM/build";
             });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "MSMPM API", Version = "V1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +60,12 @@ namespace MSMBackend
             {
                 app.UseDeveloperExceptionPage();
             }
+           
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MSMPM API");
+            });
 
             app.UseHttpsRedirection();
 
