eef7fb789984fc6436036dd7809ba9483ebbf9d1
kennethcole5 <Kenneth.cole@selu.edu>
2020-11-07 16:02:48 -0600

    Mappings for Users and Roles Created
    Used Automapper to create the mappings for UserRolesDto, LoginDto, and CreateUserDto

--- msmpm/Profiles/PropertiesProfile.cs
+++ msmpm/Profiles/PropertiesProfile.cs
@@ -12,7 +12,9 @@ namespace MSMBackend.Profiles
     public class PropertiesProfile : Profile
     {
         public PropertiesProfile()
-        {               //Source Object ---> Destination Object
+        {               
+            //Source Object ---> Destination Object
+
             CreateMap<Property, PropertyReadDto>();
             
             CreateMap<PropertyCreateDto, Property>();
@@ -20,6 +22,7 @@ namespace MSMBackend.Profiles
             CreateMap<PropertyUpdateDto, Property>();
 
             CreateMap<Property, PropertyUpdateDto>();
+
         }
 
     }
