4b471ff75d2af4d0315d21b444390e369793f4d0
Cameron Bonura <cameron.bonura@selu.edu>
2020-11-07 15:38:24 -0600

    Created and moved Dto's

---
+++ msmpm/Dtos/UsersDto/UserRolesDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSMBackend.Dtos.UsersDto
+{
+    public class UserRolesDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public IList<string> UserRoles { get; set; }
+    }
+}
