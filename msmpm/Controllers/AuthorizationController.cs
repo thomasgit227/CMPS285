440f9ba6747b4dcd4b29165048596de034659809
Cameron Bonura <cameron.bonura@selu.edu>
2020-11-07 21:13:13 -0600

    AuthController

---
+++ msmpm/Controllers/AuthorizationController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MSMBackend.Data.Entity;
+using MSMBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSMBackend.Controllers
+{
+    [Route("/api/auth")]
+    [ApiController]
+    public class AuthorizationController
+    {
+        private readonly PropertyContext propertyContext;
+        private readonly UserManager<User> userManager;
+        private readonly RoleManager<Role> roleManager;
+        private readonly SignInManager<User> signInManager;
+
+        [HttpPost("login")]
+        public async Task<ActionResult> LoginAsync(LoginDto dto)
+        {
+            var user = await userManager.FindByNameAsync(dto.Username);
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            var result = await signInManager.CheckPasswordSignInAsync(user, dto.Password, true);
+            if (!result.Succeeded)
+            {
+                return BadRequest();
+            }
+
+            await signInManager.SignInAsync(user, false, "Password");
+
+            var roles = await userManager.GetRolesAsync(user);
+
+            return Ok(new UserRoleDto
+            {
+                Id = user.Id,
+                Username = user.UserName,
+                UserRoles = roles
+            }); ;
+        }
+        [HttpPost("create")]
+        public async Task<ActionResult> Create(CreateUserDto dto)
+        {
+            var user = new User { UserName = dto.Username };
+            await userManager.CreateAsync(user, dto.Password);
+            await userManager.AddToRoleAsync(user, dto.Role);
+            return Ok();
+        }
+    }
+}
