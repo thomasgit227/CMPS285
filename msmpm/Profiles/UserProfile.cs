eaea37df7d8d25eb4c1c08d18d8727e4d917ef92
kennethcole5 <Kenneth.cole@selu.edu>
2020-11-07 21:14:54 -0600

    Created Mappings
    Mapping for user roles

---
+++ msmpm/Profiles/UserProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MSMBackend.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MSMBackend.Models;
+using Microsoft.EntityFrameworkCore.Update.Internal;
+using MSMBackend.Data.Entity;
+using MSMBackend.Dtos.UserDto;
+
+namespace MSMBackend.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {               //Source Object ---> Destination Object
+            CreateMap<User, CreateUserDto>();
+
+            CreateMap<User, LoginDto>();
+
+            CreateMap<User, UserRoleDto>();
+
+        }
+    }
+}
